c558849eccde4968bf07262213dc08f649c88ea5
Denys Churchyn <dodenisa@gmail.com>
2024-09-21 23:29:44 +0300

    hacking favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
           name="description"
           content="Generate beautiful book covers about your pain"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="./favicon.ico" />
       </Head>
       <div className="min-h-screen min-w-[540px] bg-gradient-to-b from-[#cfd9df] to-[#e2ebf0]">
         <main className="container mx-auto p-4">
