4d766b2475ebb57fb155c64479991e34c59d79df
Denys Churchyn <dodenisa@gmail.com>
2024-09-21 16:04:30 +0300

    replacing create react app with create t3 and next js

--- web/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type {Config} from "tailwindcss";
-
-export default {
-  content: ["./src/**/*.{ts,tsx}", './public/index.html'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-} satisfies Config;
