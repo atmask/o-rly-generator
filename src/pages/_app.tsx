dd69e8a589cff973927726a0d47b3cbf5b3d90ab
Denys Churchyn <dodenisa@gmail.com>
2024-09-22 12:18:24 +0300

    added GA and posthog

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,16 @@ import { GeistSans } from "geist/font/sans";
 import { type AppType } from "next/app";
 
 import "~/styles/globals.css";
+import { GoogleAnalytics } from "~/components/meta/GoogleAnalytics";
+import PostHogAnalyticsProvider from "~/components/meta/PostHogAnalytics";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <div className={GeistSans.className}>
-      <Component {...pageProps} />
+      <PostHogAnalyticsProvider>
+        <Component {...pageProps} />
+      </PostHogAnalyticsProvider>
+      <GoogleAnalytics gaId="G-VB63CNRL7H" />
     </div>
   );
 };
