eb0fb0796310facdc816921552d455d2e8899d7d
Denys Churchyn <dodenisa@gmail.com>
2025-04-14 10:20:22 +0300

    chore: slightly change bg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <OrlyHead />
-      <div className="flex min-h-screen min-w-[540px] flex-col bg-gradient-to-b from-[#cfd9df] to-[#e2ebf0]">
+      <div className="flex min-h-screen min-w-[540px] flex-col bg-gray-100">
         <main className="container mx-auto p-4">
           <CoverEditor />
         </main>
